ea4ae06cf90a8b34855f775fa31c3e17abd8ca41
ellieheo <sunheeheo10@gmail.com>
2022-01-05 19:54:46 +0900

    어제보다 나은 오늘

--- Programmers/문자열 다루기 기본(1.5.y)/answer.js
+++ Programmers/문자열 다루기 기본(1.5.y)/answer.js
@@ -4,14 +4,32 @@ console.log(regax.test(s));
 
 function solution(s) {
   let regax = /^[0-9]/g;
-  return regax.test(s);
+  if (!(s.length == 4 || s.length == 6)) {
+    return false;
+  } else {
+    for (let i = 0; i < s.length; i++) {
+      if (!regax.test(s)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 }
-// for (let i = 0; i < s.length; i++) {
-//   if (!regax.test(s)) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// }
 
 //pass :)
+//다른분이 푸신것
+function solution(s) {
+  if (!(s.length == 4 || s.length == 6)) {
+    return false;
+  } else {
+    const tmp = s.split("");
+    for (let i = 0; i < tmp.length; i++) {
+      if (isNaN(tmp[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+//isNAN()함수는 어떤 값이 NaN인지 판별(Not a Number)
