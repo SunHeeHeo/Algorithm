9a2315dd6abbe67692d757fcc841dcb0b50923f6
ellieheo <sunheeheo10@gmail.com>
2022-01-08 23:38:31 +0900

    어제보다 나은 오늘

---
+++ Programmers/수박수박수박수박수박수?/answer.js
@@ -0,0 +1,15 @@
+let n = 4;
+function solution(n) {
+  let answer = "";
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      answer += "수";
+    } else {
+      answer += "박";
+    }
+  }
+  return answer;
+}
+solution(n);
+
+//mission completed :)
